662069069f238f250615e76d85c41aa0d221a291
Nguyen Mai Linh <nguyenmailinhnana@gmail.com>
2026-04-12 13:07:39 +0700

    fix AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
